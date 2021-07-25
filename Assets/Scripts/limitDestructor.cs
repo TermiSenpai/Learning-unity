be41624ffa9d1b8b5021f180e6e250b3acb42011
Termi <escarpaprieto@gmail.com>
2021-07-25 19:44:47 +0200

    Entity destructor

---
+++ Assets/Scripts/limitDestructor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class limitDestructor : MonoBehaviour
+{
+    float limitUp = 30;
+    float limitDown = -14;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.z > limitUp || transform.position.z < limitDown)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
