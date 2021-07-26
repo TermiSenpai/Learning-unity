97a35580ec847f7e7193c0c8330819b67663ae5f
Termi <escarpaprieto@gmail.com>
2021-07-26 18:14:02 +0200

    Random entity spawn

---
+++ Assets/Scripts/spawnElementsGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawnElementsGenerator : MonoBehaviour
+{
+    public GameObject[] animalArray;
+    public int indexAnimal;
+    public int spawnPositionZ;
+    public int spawnPositionX;
+
+    float waitStart = 2.0f;
+    float waitGeneration = 2.0f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("animalPositionGenerator", waitStart, waitGeneration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void animalPositionGenerator()
+    {
+        indexAnimal = Random.Range(0, animalArray.Length);
+        spawnPositionX = Random.Range(-15, 15);
+        spawnPositionZ = Random.Range(29, 15);
+        Vector3 positionGenerator = new Vector3(spawnPositionX, 0, spawnPositionZ);
+
+        Instantiate(animalArray[indexAnimal], positionGenerator, animalArray[indexAnimal].transform.rotation);
+    }
+
+}
